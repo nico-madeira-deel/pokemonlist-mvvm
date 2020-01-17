c068ecc6940346d38a0563145096d7d921d760a4
nicollemadeira <nicollemguimaraes@gmail.com>
2020-01-17 12:09:47 -0300

    feat(teste): incluir teste de UI no app

---
+++ AppNicoMVVM/DeStringers/AccessibilityIdentifier.swift
@@ -0,0 +1,18 @@
+//
+//  AccessibilityIdentifier.swift
+//  AppNicoMVVM
+//
+//  Created by Tag Livros on 16/01/20.
+//  Copyright © 2020 Pedro Ullmann. All rights reserved.
+//
+
+import Foundation
+
+enum AccessibilityIdentifier: String {
+    case pokemonName = "pokemonName"
+    case pokemonInfo = "pokemonInfo"
+    case pokemonImage = "pokemonImage"
+    case detailPokemonTableCell = "detailPokemonTableCell"
+    case pokemonListTableView = "pokemonListTableView"
+    case goBackToList = "Pokedex"
+}
